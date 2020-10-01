0c28834d33eb7e9fd6a1869d7e89066cc1b16478
barbehugo28 <barbehugo28@users.noreply.github.com>
2020-10-01 10:29:34 +0200

    Update fibonacci_heap.py

--- fibonacci_heap.py
+++ fibonacci_heap.py
@@ -46,40 +46,41 @@ class FibonacciHeap(Heap):
     et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
     """
 
+    class Node:
+        def __init__(self, value, left, right):
+            self.value = value
+            self.left = left
+            self.right = right
+
+    root_list = None
+    min_node = None
+
+    total_nodes = 0
+
     def insert(self, value: int) -> None:
         """
         Ajoute une valeur dans l'arbre
         """
-        pass
 
     def find_min(self) -> int:
         """
         Retourne la valeur minimum dans l'arbre
         """
-        pass
+        def find_min(self):
+            return self.min_node
 
     def delete_min(self) -> int:
         """
         Supprime et retourne la valeur minimum dans l'arbre
         """
-        pass
 
     def merge(self, fibonnaci_heap: Heap) -> None:
         """
         Fusionne deux arbres
         """
-        pass
-
-heap1 = {
-    0: [2, 4, 3],
-    1: [6],
-    2: [6, 7],
-    3: [6, 9, 7],
-    4: [3]
-}
 
 heap = FibonacciHeap()
-heap.insert(3)
-heap.insert(5)
+heap.insert(42)
+heap.find_min()
 
 print(heap)
